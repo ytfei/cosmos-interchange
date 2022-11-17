eb54e24facfc76a23da4db553ca5db7507f49b83
Mason <mason.p.yang@gmail.com>
2022-11-17 11:28:24 +0800

    fix: missing func

--- x/dex/types/keys.go
+++ x/dex/types/keys.go
@@ -1,5 +1,7 @@
 package types
 
+import fmt "fmt"
+
 const (
 	// ModuleName defines the module name
 	ModuleName = "dex"
@@ -27,4 +29,8 @@ var (
 
 func KeyPrefix(p string) []byte {
 	return []byte(p)
+}
+
+func OrderBookIndex(portID string, channelID string, sourceDenom string, targetDenom string) string {
+	return fmt.Sprintf("%s-%s-%s-%s", portID, channelID, sourceDenom, targetDenom)
 }
